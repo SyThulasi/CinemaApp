26ce947d80ffde1a33f43716f45652be86f8d6bd
Yogeswaran Thulasiyan <73667144+SyThulasi@users.noreply.github.com>
2023-08-23 07:54:17 +0530

    new Backend updated error solved

--- CinemaApp_BackEnd/cinemaApp_ms/src/test/java/com/example/cinemaApp/CinemaAppMsApplicationTests.java
+++ CinemaApp_BackEnd/cinemaApp_ms/src/test/java/com/example/cinemaApp/CinemaAppMsApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class CinemaApp1ApplicationTests {
+class CinemaAppMsApplicationTests {
 
 	@Test
 	void contextLoads() {
